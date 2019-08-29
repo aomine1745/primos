dd57babdbdb8c6c5025a7648fae1e0791570fd70
aomine <xomarxpl07@outlook.es>
2019-08-29 15:02:58 -0500

    add mig

--- js/app.js
+++ js/app.js
@@ -1,22 +1,37 @@
+// Interacción con el DOM
 const contentPrimos = document.getElementById('content-primos');
+const numberUI = document.getElementById('number');
 let limitOfNumber = parseInt(prompt('Ingrese el límite de números a hallar'));
-const number = document.getElementById('number');
-number.innerHTML = limitOfNumber;
-let primo = 1;
+
+// Control de errores
+if(isNaN(limitOfNumber)){
+	contentPrimos.innerHTML = `<li>El limite es invalido</li>`
+	numberUI.innerHTML = `Ingresaste un texto 😥`
+	throw new Error('NOPE :V')
+}
 
 if(limitOfNumber <= 0) {
-	contentPrimos.innerHTML += 'El límite no es válido'
-}else {
-	do {
-		let counter = 1;
-		primo++;
-		do {
-			counter++;
-		}while(primo % counter != 0);
-		if(counter == primo) {
-			contentPrimos.innerHTML += `<li class='content-primos-number'>${primo}</li>`;
-		}else {
-			limitOfNumber++;
-		}
-	}while(limitOfNumber + 1 != primo); 
+	contentPrimos.innerHTML = '<li>El limite es invalido</li>'
+	numberUI.innerHTML = 'NOPE :V'
+	throw new Error('NOPE x2 :V')
 }
+
+//Inicio del script
+numberUI.innerHTML = limitOfNumber;
+let numeroPrimo = 1;
+
+do {
+	let number = 1;
+	numeroPrimo++;
+	do {
+		number++;
+	}while(numeroPrimo % number != 0);
+
+	if(number == numeroPrimo) {
+		contentPrimos.innerHTML += `<li class='content-primos-number'>${numeroPrimo}</li>`;
+	}else {
+		limitOfNumber++;
+	}
+
+}while(limitOfNumber + 1 != numeroPrimo); 
+
